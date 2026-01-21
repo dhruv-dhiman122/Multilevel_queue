8960f52b2f6e7efe78dcd1a3ce4af5c512a11a74
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-01-21 15:26:27 +0530

    added the code file structure for the queue data structure

--- src/queue.c
+++ src/queue.c
@@ -1,3 +1,25 @@
 /*
  * This is a implementation for queue
  * */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//========================= space for user defined macro ================================//
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+//========================= space for user defined variable ==============================//
+
+
+//========================= space for user defined function =============================//
+
+
+
+//======================== space for main function ======================================//
+int main() { 
+
+	exit(EXT_SUCCESS);
+}
