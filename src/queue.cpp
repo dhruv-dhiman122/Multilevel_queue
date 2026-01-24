438de2a8ee2dbeacb72623b0ace10d6fa8576d3b
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-01-24 21:37:16 +0530

    Added the file for queue in cpp

---
+++ src/queue.cpp
@@ -0,0 +1,22 @@
+
+
+#include <iostream>
+#include <stdlib.h>
+#include <queue>
+
+//============================== Space for macro =========================
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+//============================== Space for class or struct =============================
+
+class queue{
+
+};
+
+//============================== Space for int main function ==========================
+int main() {
+
+  exit(EXT_SUCCESS);
+}
