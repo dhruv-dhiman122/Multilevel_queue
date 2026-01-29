95568b406541a87458972a027b5d4d25e3d6142f
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-01-30 00:14:10 +0530

    Added the user defined variable for process

--- src/sjf.c
+++ src/sjf.c
@@ -13,6 +13,14 @@
 
 //========================= Space for struct ===============================//
 
+struct Process {
+  long long id;
+  float burst_time;
+  float arrival_time;
+  float completion_time;
+  float wait_time;
+  float turnaround_time;
+};
 
 //============================ Space for only main function =======================//
 int main(int argc, char* argv[]) {
