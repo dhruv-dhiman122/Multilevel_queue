6064270d658332abd043f4028c0d920b25b73307
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-01-30 00:45:12 +0530

    Made the function for sorting the burst time of the processes

--- src/sjf.c
+++ src/sjf.c
@@ -10,6 +10,19 @@
 
 //======================== Sapce for user defined function ==========================/
 
+void Sort(Struct Process process[], int number) {
+  for(int i = 0; i < number; i++) {
+    for(int j = 0; j <number-i-1; j++) {
+      if(process[j].burst_time > process[j+1].burst_time) {
+        float temp = process[j+1].burst_time;
+        process[j].burst_time = temp;
+        process[j+1].burst_time = process[j];
+      }
+    }
+  }
+}
+
+
 
 //========================= Space for struct ===============================//
 
@@ -24,7 +37,21 @@ struct Process {
 
 //============================ Space for only main function =======================//
 int main(int argc, char* argv[]) {
+  
+  if(argc < 2) {
+    printf("THERE MUST BE MORE THAN 1 VARIABLE!!");
+    exit(EXT_FAILURE);
+  }
+   
+  int number = (int)argv[2];
+  struct Process* process = (struct Process*)malloc(number * sizeof(struct Process));
+  for(int i = 0; i < number; i++) {
+    printf("Enter the %d process's arrival time\n",i+1);
+    scanf("%f",&process[i].arrival_time);
+    printf("Enter the %d process's burst time\n",i+1);
+    scanf("%f",&process[i].burst_time);
+  }
 
-
+  free(process);
   exit(EXT_SUCCESS);
 }
