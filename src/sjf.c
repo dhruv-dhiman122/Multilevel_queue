bc19eeb64d44df3738c34309bb1862bc9baf1322
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-01-30 00:11:57 +0530

    Made the file for sjf and added the basic plan for code

---
+++ src/sjf.c
@@ -0,0 +1,22 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//====================== Space for macro ===========================//
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+//======================== Sapce for user defined function ==========================/
+
+
+//========================= Space for struct ===============================//
+
+
+//============================ Space for only main function =======================//
+int main(int argc, char* argv[]) {
+
+
+  exit(EXT_SUCCESS);
+}
