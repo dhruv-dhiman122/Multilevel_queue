02b2e57c4e5c1990cf981e41c705932cd8fa640c
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-01-30 16:51:48 +0530

    made the file for process control block and made the basic structure for coding

---
+++ src/process_control_block.c
@@ -0,0 +1,21 @@
+/*
+ This file is a implematation for process control block.
+ The idea behind is that we need a track of process when made from either file
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//============================= space for macro or union =====================
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+//============================== space for function made by the user ========================
+
+
+//=========================== space only for main function =========================
+int main() {
+
+	exit(EXT_SUCCESS);
+}
