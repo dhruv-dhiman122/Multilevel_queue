5fcd0ca63f6c436e265337c52e0ba39c4b0bcae7
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-02-01 05:10:14 +0530

    Made the .h file for process control block

---
+++ src/process_control_block.h
@@ -0,0 +1,19 @@
+#ifndef PROCESS_CONTROL_BLOCK_H
+#define PROCESS_CONTROL_BLOCK_H
+
+typedef struct {
+	int porcess_id;
+	int program_counter;
+	int priority;
+	float arrival_time;
+	float burst_time;
+	float completion_time;
+	float wait_time;
+	float turnaround_time;
+} PCB;
+
+PCB* create_process(int process_id,int priority ,float arrival_time, float burst_time);
+void update_process_counter(PCB* process, int counter_value);
+void display_process(PCB* process);
+void free_process(PCB* process);
+#endif
