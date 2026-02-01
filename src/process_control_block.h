71246cbf7ab477c941348355b822552bcc6500ae
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-02-01 17:40:38 +0530

    Added the default value for create_process function

--- src/process_control_block.h
+++ src/process_control_block.h
@@ -12,7 +12,7 @@ typedef struct {
 	float turnaround_time;
 } PCB;
 
-PCB* create_process(int process_id,int priority ,float arrival_time, float burst_time);
+PCB* create_process(int process_id,int priority = 0 ,float arrival_time = 0.0, float burst_time = 0.0);
 void update_process_counter(PCB* process, int counter_value);
 void display_process(PCB* process);
 void free_process(PCB* process);
