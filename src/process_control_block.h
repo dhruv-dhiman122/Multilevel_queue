4817655a88cc0ea82fc7241e32b2e34d036e6cc2
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-02-01 18:00:21 +0530

    Made some small changes to the file

--- src/process_control_block.h
+++ src/process_control_block.h
@@ -13,7 +13,10 @@ typedef struct {
 } PCB;
 
 PCB* create_process(int process_id,int priority,float arrival_time, float burst_time);
+
 void update_process_counter(PCB* process, int counter_value);
+
 void display_process(PCB* process);
+
 void free_process(PCB* process);
 #endif
