e785ee6cea1a2b9ce22bee3751f0cdc38ca28317
Dhruv Dhiman <dhruvdhiman770@gmail.com>
2026-02-06 10:27:46 +0530

    Changed from program_counter to process_counter

--- src/shared_variable.h
+++ src/shared_variable.h
@@ -1,8 +1,8 @@
 #ifndef SHARED_H_
 #define SHARED_H_
 
-extern int sjf_program_counter;
-extern int fcfs_program_counter;
+extern int sjf_process_counter;
+extern int fcfs_process_counter;
 extern int queue_status;
 
 #endif 
