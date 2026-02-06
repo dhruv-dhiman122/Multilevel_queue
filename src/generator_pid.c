7570968360da59b385ef06cc0ff4cd4db65813f8
Dhruv Dhiman <dhruvdhiman770@gmail.com>
2026-02-06 10:30:35 +0530

    Made a new file to generate PID and also keep track of different process counter

---
+++ src/generator_pid.c
@@ -0,0 +1,31 @@
+/*
+ *  this file is the implemnation for generating pid for the process
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//============================ Space for macro ============================
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+//============================= Space for user defined function ================
+
+void sjf_process() {
+
+}
+
+void fcfs_process() {
+
+}
+
+void totalProcess() {
+
+}
+
+//============================= Space for main function ==============================
+int main() {
+
+  exit(EXT_SUCCESS);
+}
