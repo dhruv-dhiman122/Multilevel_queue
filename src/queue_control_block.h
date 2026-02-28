3cbdf31e71d108493074800bc0b20d9a6b000cac
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-02-28 22:48:10 +0530

    Made the file for queue control block

---
+++ src/queue_control_block.h
@@ -0,0 +1,13 @@
+/*
+ *  This file is used for queue control block 
+ *  The idea of queue control block is that its used for maintaining the internals of the queue 
+ *  That which is containing the process in each queue
+ * */
+
+
+#ifndef QUEUE_CONTROL_BLOCK_H
+#define QUEUE_CONTROL_BLOCK_H
+
+//TODO add the the queue status for process
+
+#endif
