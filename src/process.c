c54c7dad1a0cbf061d7bc2bdfb942693c41db739
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-03-14 13:49:23 +0530

    Made a new file for adding the implementation of process.h file

---
+++ src/process.c
@@ -0,0 +1,23 @@
+/*
+ * ============================================================================
+ *            The aim of the file is to make a common ground 
+ *            for simulation to make process.
+ * ============================================================================
+ * */
+
+#include <stdio.h>
+#include "process.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+// ============================ Space for defining macro ======================
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+// ============================= Space for defining user function ==========================
+
+int main() {
+
+  return 0;
+}
