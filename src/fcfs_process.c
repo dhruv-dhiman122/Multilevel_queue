50b0cb1b790a67144a73b27eefe3204edcf82369
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-03-14 14:51:53 +0530

    made the file for processing fcfs type processes

---
+++ src/fcfs_process.c
@@ -0,0 +1,29 @@
+/*
+ *  =====================================================
+ *                This file is implementation
+ *                for fcfs processes
+ *  =====================================================
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "process.h"
+#include <unistd.h>
+
+// ================== Space for macro ========================
+
+#define EXT_SUCCESS 0
+#define EXT_FAILURE 1
+
+// ================== Space for user defined function ==============
+
+
+// =============== space for main function ======================
+
+int main() {
+  printf("Enter the arrival time and burst time for processes\n");
+  for(int i = 0; i < fcfs_type_process; i++) {
+
+  }
+  exit(EXT_SUCCESS);
+}
