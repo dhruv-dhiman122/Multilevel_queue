c8600b2ea76c431e1c79a89421d8bc3896de0114
dhruv dhiman <dhruvdhiman770@gmail.com>
2026-03-14 14:13:28 +0530

    Add the code section for asking number of process and type of process

--- src/process.c
+++ src/process.c
@@ -17,7 +17,16 @@
 
 // ============================= Space for defining user function ==========================
 
+
+
+// =========================== space for main function only ================================
 int main() {
+  printf("Enter the number of process you want to create\n");
+  int user_process_number;
+  scanf("%d",&user_process_number);
+  printf("Enter the type of process you want to make; 1 for FCFS type and 2 for SJF\n");
+  int process_type;
+  scanf("%d",&process_type);
 
   return 0;
 }
